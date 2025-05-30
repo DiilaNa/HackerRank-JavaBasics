9d46b5dedaf1f4f9d47c718551f71d8d97967f43
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-30 09:17:05 +0530

    Case 01

--- src/main/java/lk/ijse/project/Main.java
+++ src/main/java/lk/ijse/project/Main.java
@@ -1,6 +1,19 @@
 package lk.ijse.project;
-public class Main {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+
+public class Main {
+    /*case-01*/
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter number: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter number: ");
+        int c = scanner.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
