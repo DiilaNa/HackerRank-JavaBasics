d1a935fc4341271234fb281b5fade59118b9d388
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-31 16:19:29 +0530

    case-06

--- src/main/java/lk/ijse/project/Main.java
+++ src/main/java/lk/ijse/project/Main.java
@@ -65,7 +65,7 @@ public class Main {
         System.out.println("================================");
         */
 
-        /*case - 05*/
+     /*   *//*case - 05*//*
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
@@ -75,6 +75,22 @@ public class Main {
             System.out.println(N +" x "+ i +" = " + r);
         }
 
-        bufferedReader.close();
+        bufferedReader.close();*/
+
+        /*case-06*/
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for(int j=0;j<n;j++){
+                a=a+b;
+                System.out.println(a+" ");
+                b=b*2;
+            }
+            System.out.println(" ");
+        }
+        in.close();
     }
 }
